499f590ab38b49bac786ec42dae7cefd3dc8fdc6
JacaP30 <jacap30@gmail.com>
2025-10-08 15:49:37 +0200

    kosmetyka - zmiana tytułu i podtytułu aplikacji

--- zaddomjp_app.py
+++ zaddomjp_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd # type: ignore
 from pycaret.clustering import load_model, predict_model # type: ignore
 import plotly.express as px # type: ignore
+from streamlit_option_menu import option_menu 
 
 
 # GŁÓWNA APLIKACJA 
@@ -11,6 +12,13 @@ import plotly.express as px # type: ignore
 # Wykorzystuje model klastrowania do grupowania uczestników.
 # ====================================================================
 
+# ustawienia globalne dla streamlit
+st.set_page_config(
+    page_title="Znajdź znajomych",
+    page_icon=":guardsman:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 
 MODEL_NAME = 'welcome_survey_clustering_pipeline_v2'# <--- NAZWA MODELU
 
@@ -40,12 +48,12 @@ def get_all_participants():
     return df_with_clusters # Zwracamy DataFrame z klastrami
 
 with st.sidebar:
-    st.header("Powiedz nam coś o sobie")
-    st.markdown("Pomożemy Ci znaleźć osoby, które mają podobne zainteresowania")
+    st.markdown("<span style='color:#4e61f3ff; font-size: 24px; font-weight: bold;'>Powiedz nam coś o sobie</span>", unsafe_allow_html=True)
+    st.markdown("Pomożemy Ci znaleźć osoby z pośród uczestników kursu, które mają podobne zainteresowania")
     # tworzy kolejne zmienne na podstawie wyborów w sidebar
-    age = st.selectbox("Wiek", ['<18', '25-34', '45-54', '35-44', '18-24', '>=65', '55-64', 'unknown'])
+    age = st.selectbox("Wiek", ['<18', '18-24', '25-34', '35-44', '45-54', '55-64', '>=65', 'unknown'])
     edu_level = st.selectbox("Wykształcenie", ['Podstawowe', 'Średnie', 'Wyższe'])
-    fav_animals = st.selectbox("Ulubione zwierzęta", ['Brak ulubionych', 'Psy', 'Koty', 'Inne', 'Koty i Psy'])
+    fav_animals = st.selectbox("Ulubione zwierzęta", ['Brak ulubionych', 'Koty', 'Psy', 'Koty i Psy', 'Inne'])
     fav_place = st.selectbox("Ulubione miejsce", ['Nad wodą', 'W lesie', 'W górach', 'Inne'])
     gender = st.radio("Płeć", ['Mężczyzna', 'Kobieta'])
 
@@ -66,13 +74,21 @@ cluster_names_and_descriptions = get_cluster_names_and_descriptions() # Pobieram
 predicted_cluster_id = predict_model(model, data=person_df)["Cluster"].values[0] # Przewidujemy ID klastra dla osoby
 predicted_cluster_data = cluster_names_and_descriptions[predicted_cluster_id] # Pobieramy dane o przewidzianym klastrze
 
-st.header(f"Najbliżej Ci do grupy {predicted_cluster_data['name']}")
+# title w kolorze niebieskim
+
+st.markdown("""
+<span style='color:#4e61f3ff; font-size: 40px; font-weight: bold;'>Znajdź znajomych wśród uczestników kursu z pomocą AI</span>
+""", unsafe_allow_html=True)
+st.markdown("Aplikacja wykorzystuje model klastrowania do znalezienia osób o podobnych zainteresowaniach i cechach demograficznych.")
+
+st.markdown(f"<span style='color:#6374f9ff; font-size: 32px;'> Szukaj znajomych wśród osób podobnych do ciebie </span>", unsafe_allow_html=True)
+st.markdown(f"<span style='color:#7fb4f5ff; font-size: 22px;'> Najbliżej Ci do grupy \"{predicted_cluster_data['name']}\"</span>", unsafe_allow_html=True)
 st.markdown(predicted_cluster_data['description'])
 same_cluster_df = all_df[all_df["Cluster"] == predicted_cluster_id]
-st.metric("Liczba twoich znajomych", len(same_cluster_df))
+st.metric("Liczba twoich potencjalnych znajomych", len(same_cluster_df))
 
 # Wyświetlamy dane o osobach z tego samego klastra
-st.header("Osoby z grupy")
+st.header("Wizualizacje dla osób z grupy")
 fig = px.histogram(same_cluster_df.sort_values("age"), x="age")
 fig.update_layout(
     title="Rozkład wieku w grupie",
